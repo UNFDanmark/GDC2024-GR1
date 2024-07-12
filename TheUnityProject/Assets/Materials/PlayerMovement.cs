12e6c917ab8309c502156f2fd18b18b6e13fa34c
Deltager <teknisk+GDC2024-GR1@game.unf.dk>
2024-07-12 20:53:04 +0200

    basic player movement

---
+++ TheUnityProject/Assets/Materials/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private Rigidbody rb;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = rb.velocity;
+        movement.z = Input.GetAxisRaw("Horizontal") * speed;
+        movement.x = Input.GetAxisRaw("Vertical") * speed;
+        rb.velocity = movement;
+    }
+}
+

340c3f7e2d6864b2cbb56494951ecaf31b4a4774
Deltager <teknisk+GDC2024-GR1@game.unf.dk>
2024-07-13 16:53:00 +0200

    stuff

--- TheUnityProject/Assets/Materials/PlayerMovement.cs
+++ TheUnityProject/Assets/Materials/PlayerMovement.cs
@@ -9,9 +9,11 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.82f;
     private Vector3 velocity;
-    
-    
-    
+    public Transform GroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+    public float JumpHight = 3f;
     void Start()
     {
         
@@ -20,13 +22,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        float X = Input.GetAxisRaw("Horizontal");
+        //WASD
+        //float X = Input.GetAxisRaw("Horizontal");
         float Z = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = transform.right * X + transform.forward * Z;
+        Vector3 move = transform.right * -1f + transform.forward * Z;
 
         controller.Move(move * speed * Time.deltaTime);
+        // Space
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+            
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(JumpHight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 

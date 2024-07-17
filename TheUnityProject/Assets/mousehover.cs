b579740b0a484b5ec1c70d72874db75b08589c92
Deltager <teknisk+GDC2024-GR1@game.unf.dk>
2024-07-17 09:20:10 +0200

    ddd

--- TheUnityProject/Assets/mousehover.cs
+++ TheUnityProject/Assets/mousehover.cs
@@ -1,22 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mousehover : MonoBehaviour
+
 {
-     
+    public GameObject hoverbotten;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        hoverbotten.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMouseEnter()
     {
-        
-        
+        hoverbotten.SetActive(true);
+    }
+    public void OnMouseExit()
+    {
+        hoverbotten.SetActive(false);
     }
 }
